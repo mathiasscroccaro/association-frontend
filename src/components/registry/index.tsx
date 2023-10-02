f596730afaba4a6e57e72c584c2c41438014a8b9
Mathias <mathias.scroccaro@gmail.com>
2023-10-02 19:22:28 -0300

    Add track to registry component

--- src/components/registry/index.tsx
+++ src/components/registry/index.tsx
@@ -6,11 +6,22 @@ import Typography from '@mui/material/Typography'
 import Grid from '@mui/material/Grid'
 import TextField from '@mui/material/TextField'
 import useMediaQuery from '@mui/material/useMediaQuery';
-import { Box, Divider, Button, Grow } from '@mui/material'
+import { Box, Divider, Button, Grow, TableContainer, Table, Paper, TableRow, TableCell, TableBody, CardMedia } from '@mui/material'
 import { useTheme } from '@mui/material/styles';
-import CounterProvider from '../context/FormProvider'
+import FormProvider from '../context/FormProvider'
 import { useContext } from 'react'
-import CounterContext from '../context/FormContext'
+import FormContext from '../context/FormContext'
+import { ActionType } from '../context/FormReducer'
+import HocPhoneField from './Form/PhoneField'
+import HocCpfField from './Form/CPFField'
+import HocRgField from './Form/RGField'
+import HocCommonField from './Form/CommonField'
+import HocCepField from './Form/CEPField'
+import { styled } from '@mui/material/styles';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import HocUploadButton from './common/UploadButton'
+import { HocImageCard } from './common/ImagePreview'
+import LogoImage from './common/LogoImage'
 
 
 
@@ -55,115 +66,114 @@ function Title() {
 }
 
 function Form() {
+    const { state, dispatch } = useContext(FormContext)
 
     return (
         <Grid container spacing={2}>
+            <Grid item xs={12}>
+                <Typography variant="h6" color="primary">Informações do RESPONSÁVEL</Typography>
+            </Grid>
             <Grid item xs={12} sm={6}>
-                <TextField
-                    fullWidth
-                    id=""
+                <HocCommonField
                     label="Nome completo"
+                    dispatch={dispatch}
+                    actionType={ActionType.updateFullName}
+                    value={state.formPayload.fullName}
                 />
             </Grid>
             <Grid item xs={12} sm={6}>
-                <TextField
-                    fullWidth
-                    id=""
-                    label="CPF"
-                />
+                <HocCpfField dispatch={dispatch} value={state.formPayload.cpf} />
             </Grid>
             <Grid item xs={12} sm={6}>
-                <TextField
-                    fullWidth
-                    id=""
-                    label="RG"
-                />
+                <HocRgField dispatch={dispatch} value={state.formPayload.rg} />
             </Grid>
             <Grid item xs={12} sm={6}>
-                <TextField
-                    fullWidth
-                    id=""
+                <HocCommonField
                     label="Identidade de gênero"
+                    dispatch={dispatch}
+                    actionType={ActionType.updateGenderIdentity}
+                    value={state.formPayload.genderIdentity}
                 />
             </Grid>
             <Grid item xs={12} sm={6}>
-                <TextField
-                    fullWidth
-                    id=""
+                <HocCommonField
                     label="Nacionalidade"
+                    dispatch={dispatch}
+                    actionType={ActionType.updateNationality}
+                    value={state.formPayload.nationality}
                 />
             </Grid>
             <Grid item xs={12} sm={6}>
-                <TextField
-                    fullWidth
-                    id=""
+                <HocCommonField
                     label="Estado civil"
+                    dispatch={dispatch}
+                    actionType={ActionType.updateMaritalStatus}
+                    value={state.formPayload.maritalStatus}
                 />
             </Grid>
             <Grid item xs={12} sm={6}>
-                <TextField
-                    fullWidth
-                    id=""
+                <HocCommonField
                     label="Email"
+                    dispatch={dispatch}
+                    actionType={ActionType.updateEmail}
+                    value={state.formPayload.email}
                 />
             </Grid>
             <Grid item xs={12} sm={6}>
-                <TextField
-                    fullWidth
-                    id=""
-                    label="Telefone celular"
-                />
+                <HocPhoneField dispatch={dispatch} value={state.formPayload.phoneNumber} />
             </Grid>
             <Grid item xs={12}>
                 <Divider />
             </Grid>
             <Grid item xs={12} sm={6}>
-                <TextField
-                    fullWidth
-                    id=""
-                    label="CEP"
-                />
+                <HocCepField dispatch={dispatch} value={state.formPayload.cep} />
             </Grid>
             <Grid item xs={12} sm={6}>
-                <TextField
-                    fullWidth
-                    id=""
+                <HocCommonField
                     label="Rua"
+                    dispatch={dispatch}
+                    actionType={ActionType.updateStreet}
+                    value={state.formPayload.street}
                 />
             </Grid>
             <Grid item xs={12} sm={6}>
-                <TextField
-                    fullWidth
-                    id=""
+                <HocCommonField
                     label="Número"
+                    dispatch={dispatch}
+                    actionType={ActionType.updateHouseNumber}
+                    value={state.formPayload.houseNumber}
                 />
             </Grid>
             <Grid item xs={12} sm={6}>
-                <TextField
-                    fullWidth
-                    id=""
+                <HocCommonField
                     label="Complemento"
+                    dispatch={dispatch}
+                    actionType={ActionType.updateComplement}
+                    value={state.formPayload.complement}
                 />
             </Grid>
             <Grid item xs={12} sm={6}>
-                <TextField
-                    fullWidth
-                    id=""
+                <HocCommonField
                     label="Bairro"
+                    dispatch={dispatch}
+                    actionType={ActionType.updateNeighborhood}
+                    value={state.formPayload.neighborhood}
                 />
             </Grid>
             <Grid item xs={12} sm={6}>
-                <TextField
-                    fullWidth
-                    id=""
+                <HocCommonField
                     label="Cidade"
+                    dispatch={dispatch}
+                    actionType={ActionType.updateCity}
+                    value={state.formPayload.city}
                 />
             </Grid>
             <Grid item xs={12} sm={6}>
-                <TextField
-                    fullWidth
-                    id=""
+                <HocCommonField
                     label="Estado"
+                    dispatch={dispatch}
+                    actionType={ActionType.updateState}
+                    value={state.formPayload.state}
                 />
             </Grid>
 
@@ -172,24 +182,76 @@ function Form() {
 }
 
 function UploadPersonalDocuments() {
-    return (<></>)
+    const { state, dispatch } = useContext(FormContext);
+
+    return (
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <Typography variant="h6" color="primary">Documentos pessoais do RESPONSÁVEL</Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <HocUploadButton dispatch={dispatch} actionType={ActionType.addPersonalDocument} />
+            </Grid>
+            <Grid item xs={12}>
+                <Box sx={{ display: 'flex', flexWrap: 'wrap', flexDirection: 'row', justifyContent: 'center' }}>
+                    {state.personalDocuments.map((value) => (
+                        <HocImageCard base64Image={value} dispatch={dispatch} actionType={ActionType.removePersonalDocument} />
+                    ))}
+                </Box>
+            </Grid>
+
+        </Grid>
+    )
 }
 
 function UploadMedicalDocuments() {
-    return (<></>)
+    const { state, dispatch } = useContext(FormContext);
+
+    return (
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <Typography variant="h6" color="primary">Documentos médicos do PACIENTE</Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <HocUploadButton dispatch={dispatch} actionType={ActionType.addMedicalDocument} />
+            </Grid>
+            <Grid item xs={12}>
+                <Box sx={{ display: 'flex', flexWrap: 'wrap', flexDirection: 'row', justifyContent: 'center' }}>
+                    {state.medicalDocuments.map((value) => (
+                        <HocImageCard base64Image={value} dispatch={dispatch} actionType={ActionType.removeMedicalDocument} />
+                    ))}
+                </Box>
+            </Grid>
+        </Grid>
+    )
 }
 
-function AboutRegistry() {
+function FormSummary() {
+    const { state } = useContext(FormContext);
+
+    const attributes = Object.keys(state.formPayload);
+
+    const rows = attributes.map((key) => {
+        const attribute = (state.formPayload as any)[key];
+        return { attribute: (state.formPayload as any)[key] }
+
+    })
+
     return (
-        <Box sx={{ pt: 2, pb: 2 }}>
-            <Typography variant="h6" color="primary">Informações do RESPONSÁVEL</Typography>
-        </Box>
+        <Container component={Paper}>
+            {rows.map((row) => (
+                <Typography variant="body1" color="initial">{row.attribute}</Typography>
+            ))}
+        </Container>
     )
 }
 
 function MainContent(props: StepProps) {
     const pages = [
-        <Form />
+        <Form />,
+        <UploadPersonalDocuments />,
+        <UploadMedicalDocuments />,
+        <FormSummary />
     ]
     return (
         <>{pages[props.step]}</>
@@ -197,15 +259,15 @@ function MainContent(props: StepProps) {
 }
 
 function NavigationButtons(props: StepProps) {
-    const { dispatch } = useContext(CounterContext)
-    
+    const { dispatch } = useContext(FormContext)
+
     const theme = useTheme()
 
     const disableBackward = props.step == 0
     const disableForward = props.step == 3
 
-    const increment = () => dispatch({ type: "INCREMENT" })
-    const decrement = () => dispatch({ type: "DECREMENT" })
+    const increment = () => dispatch({ type: ActionType.incrementStep })
+    const decrement = () => dispatch({ type: ActionType.decrementStep })
 
     return (
         <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', pt: 3, maxWidth: theme.breakpoints.values.sm, m: 'auto' }}>
@@ -220,16 +282,14 @@ function NavigationButtons(props: StepProps) {
 }
 
 function Registry() {
-    const { state } = useContext(CounterContext)
-    const step = state.count
-
-    console.log(step, state)
+    const { state } = useContext(FormContext)
+    const step = state.step
 
     return (
         <Container maxWidth="md">
+            <LogoImage />
             <Title />
             <HorizontalStepper step={step} />
-            <AboutRegistry />
             <MainContent step={step} />
             <NavigationButtons step={step} />
         </Container>
@@ -241,9 +301,9 @@ function Registry() {
 export default function () {
 
     return (
-        <CounterProvider>
-            <Registry />           
-        </CounterProvider>
+        <FormProvider>
+            <Registry />
+        </FormProvider>
 
     );
 }
